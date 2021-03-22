a8e8edef5203cb9cdbf77f7342148379291d9fad
Tommy Liu <t.liu625@gmail.com>
2021-03-22 17:11:26 -0400

    deploying to heroku

---
+++ secrets.js
@@ -0,0 +1,9 @@
+const AWS_ID = 'AKIAQKA5AC2KMVDN6YG3';
+const AWS_SECRET = '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';
+const AWS_BUCKET_NAME = 'payuppal-bucket';
+
+module.exports = {
+  AWS_ID,
+  AWS_SECRET,
+  AWS_BUCKET_NAME,
+};
