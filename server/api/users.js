99d4fffef4cdde53bc690f4029f0f69a9c33d1f6
Zoran Bajic <zoranbajic@pm.me>
2021-03-24 14:26:25 -0500

    chore: add route to edit user information

--- server/api/users.js
+++ server/api/users.js
@@ -3,11 +3,39 @@ const router = require('express').Router();
 
 // GET single user
 // api/users/userId
-
 router.get('/:userId', async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.userId);
     res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// POST single user
+// api/users/
+router.post('/', async (req, res, next) => {
+  try {
+    const user = await User.Create(req.body);
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/', async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const { userId } = req.params;
+
+    const salt = User.generateSalt();
+    const hashedPw = User.encryptPassword(password, salt);
+
+    await User.update(
+      { email, salt, password: hashedPw },
+      { where: { id: userId } }
+    );
+    res.status(204).end();
   } catch (err) {
     next(err);
   }
