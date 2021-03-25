660e582e5e51658bd55b97e7910dfa1310c18960
Jason Liao <jlfliao@gmail.com>
2021-03-25 15:58:32 -0500

    feat: a user can now view debts, grouped by the name of their friends

--- client/components/ViewDebts.js
+++ client/components/ViewDebts.js
@@ -25,7 +25,6 @@ const ViewDebts = () => {
   useEffect(() => {
     const fetchDebts = async () => {
       let { data } = await axios.get('api/debts/displayDebts');
-
       const total = calcTotalOwed(data);
 
       setDebts(data);
@@ -35,28 +34,56 @@ const ViewDebts = () => {
     fetchDebts();
   }, [totalOwed]);
 
-  console.log(debts);
+  // get a unique list of borrowers
+  const listOfBorrowers = [];
+  const listOfBorrowerIds = [];
 
+  debts.forEach((balanceObj) => {
+    if (!listOfBorrowerIds.includes(balanceObj.friendId)) {
+      listOfBorrowerIds.push(balanceObj.friendId);
+      listOfBorrowers.push({
+        friendId: balanceObj.friendId,
+        friendName: balanceObj.friend.name,
+      });
+    }
+  });
+
+  // console.log('list of borrowers: ', listOfBorrowers);
+  // const DarylDebts = debts.filter((balance) => balance.friendId === 4);
+  // console.log('Daryl owes these balances: ', DarylDebts);
+  // console.log('for a total of: ', calcTotalOwed(DarylDebts) / 100);
   return (
     <div>
       <h2>You are owed a total of: ${totalOwed / 100}</h2>
-      <ul>
-        {debts.length > 0
-          ? debts.map((debt) => (
-              <div key={debt.id} className={debt.paid ? 'paid' : ''}>
-                {debt.friend.name} owes {debt.balance / 100}
-                <button>Send Reminder</button>
-                <button
-                  onClick={async () =>
-                    setTotalOwed(calcTotalOwed(await markPaid(debt.id)))
-                  }
-                >
-                  Mark as Paid
-                </button>
-              </div>
-            ))
-          : 'Loading debts...'}
-      </ul>
+      <div>
+        {listOfBorrowers.map((borrower) => {
+          let debtsOwedByFriend = debts.filter(
+            (balance) => balance.friendId === borrower.friendId
+          );
+          return (
+            <div className='border'>
+              <p>
+                {borrower.friendName} - {calcTotalOwed(debtsOwedByFriend)}
+              </p>
+              {debtsOwedByFriend.map((debt) => (
+                <div>
+                  <p className={debt.paid ? 'paid' : ''}>
+                    {debt.id} - {debt.balance}
+                  </p>
+                  <button>Send Reminder</button>
+                  <button
+                    onClick={async () =>
+                      setTotalOwed(calcTotalOwed(await markPaid(debt.id)))
+                    }
+                  >
+                    Mark as Paid
+                  </button>
+                </div>
+              ))}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
