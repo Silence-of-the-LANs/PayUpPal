9613bf3ae8e472e6ad3204dab05c29b21fe2b242
Jason Liao <jlfliao@gmail.com>
2021-03-29 15:53:37 -0500

    updated route for debts to display debts of the logged in user

--- server/api/friends.js
+++ server/api/friends.js
@@ -4,15 +4,17 @@ const router = require('express').Router();
 // api/friends/displayFriends route
 router.get('/displayFriends', async (req, res, next) => {
   try {
-    // const userId = req.session.user
+    console.log(req.session);
+    const userId = req.session.passport.user;
 
     const friends = await Friend.findAll({
       where: {
-        // userId: userId,
+        userId: userId,
       },
       order: [['name', 'asc']],
     });
 
+    console.log(friends);
     res.json(friends);
   } catch (err) {
     next(err);
@@ -25,20 +27,20 @@ router.post('/addFriend', async (req, res, next) => {
     const name = req.body.name;
     const email = req.body.email;
     const phone = req.body.phone;
-    // const userId = req.session.user
+    const userId = req.session.passport.user;
 
     await Friend.findOrCreate({
       where: {
         name: name,
         email: email,
         phone: phone,
-        // userId: userId,
+        userId: userId,
       },
     });
 
     const updatedFriends = await Friend.findAll({
       where: {
-        // userId: userId,
+        userId: userId,
       },
       order: [['name', 'asc']],
     });
@@ -52,18 +54,18 @@ router.post('/addFriend', async (req, res, next) => {
 // api/friends/removeFriend/:friendId route
 router.delete('/removeFriend/:friendId', async (req, res, next) => {
   try {
-    // const userId = req.session.user
+    const userId = req.session.passport.user;
 
     await Friend.destroy({
       where: {
         id: req.params.friendId,
-        // userId: userId,
+        userId: userId,
       },
     });
 
     const updatedFriends = await Friend.findAll({
       where: {
-        // userId: userId,
+        userId: userId,
       },
       order: [['name', 'asc']],
     });
